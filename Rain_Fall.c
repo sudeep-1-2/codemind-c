3b242f6410f7a26911fd3161a5b7b3c18ffe9043
sudeep-1-2 <147488406+sudeep-1-2@users.noreply.github.com>
2024-01-02 06:43:27 +0530

    #include<stdio.h> int main() {     int x;     scanf("%d",&x);     if(x<3)     {         printf("LIGHT");     }     else if(x>=3 &&x<7)     {         printf("MODERATE");     }     else if(x>=7)     {         printf("HEAVY");     }     else     {         printf("Invalid input");     } }

---
+++ Rain_Fall.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int x;
+    scanf("%d",&x);
+    if(x<3)
+    {
+        printf("LIGHT");
+    }
+    else if(x>=3 &&x<7)
+    {
+        printf("MODERATE");
+    }
+    else if(x>=7)
+    {
+        printf("HEAVY");
+    }
+    else
+    {
+        printf("Invalid input");
+    }
+}
